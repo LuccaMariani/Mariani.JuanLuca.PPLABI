e598462d05698d7d28cd236883fc9f482a5e816e
LuccaMariani <juanlucamariani@gmail.com>
2020-10-21 11:37:10 -0300

    subo parcial Mariani

--- dataStore.h
+++ dataStore.h
@@ -1,21 +1,21 @@
-
-
 #ifndef DATASTORE_H_INCLUDED
 #define DATASTORE_H_INCLUDED
 
-char HdescripcionTipo[5][11]={"Enduro","Chopera","Scooter","Ciclomotor"};
-int HidTipo[]={100,101,102,103};
 
-char Hcolores[6][11]={"Gris","Negro","Blanco","Azul","Rojo"};
-int HidColores[]={200,201,202,203,204};
+//char HdescripcionTipo[5][11]={"Enduro","Chopera","Scooter","Ciclomotor"};
 
+//char Hcolores[6][11]={"Gris","Negro","Blanco","Azul","Rojo"};
+
+/*
 char HnombreServicios[5][11]={"Limpieza","Ajuste","Balanceo","Cadena"};
-int HidServicios[]={300,301,302,303};
+
 int HprecioServicios[]={250,300,17,190};
 
 
 char Hmarcas[5][10]={"Honda","Kawasaki","Mistu","Honda"};
 int HidMotos[]={1000,1001,1002,1003,1004};
 int Hcilindrada[]={1000,1001,1002,1003,1004};
+*/
 
 #endif // DATASTORE_H_INCLUDED
+
